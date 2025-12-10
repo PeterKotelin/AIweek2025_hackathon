ad6d46edde9ff28c8d1d7a13028dbac79d38f94f
darkselia <darkselia@users.noreply.github.com>
2025-12-10 18:01:51 +0700

    add image classification endpoint to FastAPI application

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from typing import Union
+import base64
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from starlette.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"It": "works"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/api/classify")
+async def classify_image(file: UploadFile = File(...)):
+    """
+    Просто возвращает файл в base64 без анализа
+    """
+    contents = await file.read()
+
+    return JSONResponse(content={
+        "success": True,
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "file_size_bytes": len(contents),
+        "image_data": base64.b64encode(contents).decode("utf-8")
+    })
